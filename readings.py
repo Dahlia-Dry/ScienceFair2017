489bf27d054c8dc88cc450237b4a22967ba9604e
Dahlia24 <dahlia.dry24@gmail.com>
2016-11-22 19:48:17 +0000

    november_22

--- readings.py
+++ readings.py
@@ -4,34 +4,29 @@ import pandas as pd
 import datetime
 adc = Adafruit_ADS1x15.ADS1115()
 GAIN = 1
-a = datetime.datetime.today()
-boolean = (a.hour == "18")
-filename ="data.txt"
+filename ="november_18.txt"
 target = open(filename, 'w')
 def timezone(date):
+	day = date.day
+	day = day - 1
 	y = date.hour
-	y = y - 5
-	value = "%s-%s-%s %s:%s:%s" %(date.year, date.month, date.day, y, date.minute, date.second)
+	y = y + 7
+	value = "%s-%s-%s %s:%s:%s" %(date.year, date.month, day, y, date.minute, date.second)
 	return value
-while boolean == False:
-	time_start = datetime.datetime.now()
+while True:
 	total = 0
 	avg = 0
-	a = datetime.datetime.today()
+	a = datetime.datetime.now()
 	b = timezone(a)
-	for i in range(1,10):
+	for i in range(1,20):
 		x = adc.read_adc(0, gain=GAIN)
 		total = total + x
-		avg = total / 10
+		avg = total / 20
 	t =str(b)
 	data = str(avg)
 	target.write(t)
-	target.write("\n")
+	target.write(",")
 	target.write(data)
 	target.write("\n")
-	time_end = datetime.datetime.now()
-	dif = time_end - time_start
-	dif_seconds = dif.seconds
-	dif_seconds = dif_seconds - (dif.microseconds/1000000)
-	time.sleep(60-dif_seconds)
+	time.sleep(60)
 
